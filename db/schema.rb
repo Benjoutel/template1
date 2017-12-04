682c377c4dd8dbc4a69b57ee6cda41fb324ce150
christophe ridarch <christophe.ridarch@gmail.com>
2017-12-04 17:28:19 +0100

    Add Patients Model

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20171204162605) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "patients", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 20171204162605) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["email"], name: "index_patients_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_patients_on_reset_password_token", unique: true, using: :btree
   end
 
 end
