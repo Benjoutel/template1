9f50ac693a4849a852b8b99bc7ac51198893520f
Benjamin <32609318+Benjoutel@users.noreply.github.com>
2017-12-05 16:50:26 +0100

    delete skip before action

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,5 +1,6 @@
 class EpisodesController < ApplicationController
-  skip_before_action :authenticate_patient!, only: [:index, :show]
+  
+  
   def show
     @episode = Episode.find_by_id(params[:id])
     @events = Event.where(episode_id: @episode.id).order(date: :desc)
