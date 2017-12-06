066aecc15cad8660713aaa4a3881abdd22bb4986
christophe <chris.l.honore@gmail.com>
2017-12-06 20:39:41 +0100

    add rdv

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,30 @@
+class AppointmentsController < ApplicationController
+
+  def new
+    @episode = Episode.find(params[:episode_id])
+    @appointment = Event.new(category: "consultation")
+    # (category: "appointment")
+    @caregivers = Caregiver.where(patient_id: @episode.patient_id).where.not(firstname: "Note")
+
+
+  end
+
+
+  def create
+    @episode = Episode.find(params[:episode_id])
+    #caregiver = Caregiver.find_by_lastname(appointment_params[:caregiver_id])
+    @appointment = Event.new(episode_id: @episode.id, caregiver_id: appointment_params[:caregiver_id] ,category:"consultation",  date: appointment_params[:date], description: appointment_params[:description])
+    if @appointment.save
+      redirect_to episode_path(@episode)
+    else
+      render :new
+    end
+  end
+
+
+private
+
+def appointment_params
+    params.require(:event).permit(:date, :description , :caregiver_id)
+  end
+end
