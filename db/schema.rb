e3dc2f79ad28058a066eb3d2ada1d81e52d13c81
christophe ridarch <christophe.ridarch@gmail.com>
2017-12-06 10:57:41 +0100

    Rework seed and event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171205084056) do
+ActiveRecord::Schema.define(version: 20171205173955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,6 @@ ActiveRecord::Schema.define(version: 20171205084056) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.string   "name"
     t.date     "date"
     t.text     "description"
     t.string   "category"
