0d2978b36471ffcf773fe0e02535fa78ad6d8cbf
christophe ridarch <christophe.ridarch@gmail.com>
2017-12-07 20:01:53 +0100

    Add document feature & change appoitment controller create function

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -13,7 +13,10 @@ class AppointmentsController < ApplicationController
   def create
     @episode = Episode.find(params[:episode_id])
     #caregiver = Caregiver.find_by_lastname(appointment_params[:caregiver_id])
-    @appointment = Event.new(episode_id: @episode.id, caregiver_id: appointment_params[:caregiver_id] ,category:"consultation",  date: appointment_params[:date], description: appointment_params[:description])
+    # @appointment = Event.new(episode_id: @episode.id, caregiver_id: appointment_params[:caregiver_id] ,category:"appointment",  date: appointment_params[:date], description: appointment_params[:description])
+    @appointment = Event.new(appointment_params)
+    @appointment.episode = @episode
+    @appointment.category = "appointment"
     if @appointment.save
       redirect_to episode_path(@episode)
     else
