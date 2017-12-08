a543ccdb0b966c15da7253d469a5e1e1c3601e16
Benjamin Moutel <benjoutel@gmail.com>
2017-12-08 16:32:30 +0100

    seed for pitch

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,17 +29,17 @@ Antecedent.destroy_all
 puts "destroy Patients"
 Patient.destroy_all
 
-puts "Create Patient Benjamin"
-benjamin = Patient.new(firstname: "Benjamin",
-  lastname: "Moutel",
-  email: 'benjamin@gmail.com',
-  password: 123456,
-  photo: File.open(Rails.root.join('db/fixtures/images/benjamin.jpg')),
-  birth_date: "1984-09-14",
+puts "Create Patient chris"
+chris = Patient.new(firstname: "christophe",
+  lastname: "Taro",
+  email: 'chris@gmail.com',
+  password: "popopo",
+  photo: File.open(Rails.root.join('db/fixtures/images/chris.jpg')),
+  birth_date: "1987-09-14",
   phone_number: "06-07-30-46-76",
   address: "27 Boulevard Stalingrad 44000 Nantes"
   )
-benjamin.save!
+chris.save!
 
 puts "Patient Create"
 
@@ -47,25 +47,25 @@ puts "Creating measures..."
 
 measure_attributes = [
   {
-    patient: benjamin,
+    patient: chris,
     name: "poids",
     value: 70,
     date: "2016-10-27"
   },
   {
-    patient: benjamin,
+    patient: chris,
     name: "poids",
     value: 70,
     date: "2017-10-27"
   },
   {
-    patient: benjamin,
+    patient: chris,
     name: "taille",
     value: 170,
     date: "2017-10-27"
   },
   {
-    patient: benjamin,
+    patient: chris,
     name: "taille",
     value: 170,
     date: "2016-10-27"
@@ -78,22 +78,22 @@ puts 'Finished!'
 
 vaccination_attributes = [
     {
-    patient: benjamin,
+    patient: chris,
     vaccin: "diphtérie",
     date: "1985-10-27"
   },
   {
-    patient: benjamin,
+    patient: chris,
     vaccin: "tétanos",
     date: "1986-02-27"
   },
   {
-    patient: benjamin,
+    patient: chris,
     vaccin: "poliomyélite",
     date: "1986-03-13"
   },
   {
-    patient: benjamin,
+    patient: chris,
     vaccin: "fièvre jaune",
     date: "2010-09-05"
   }
@@ -103,19 +103,19 @@ vaccination_attributes = [
 Vaccination.create!(vaccination_attributes)
 
 allergy_attributes = [  {
-    patient: benjamin,
+    patient: chris,
     name: "accarien"
   },
   {
-    patient: benjamin,
+    patient: chris,
     name: "pollen"
   },
   {
-    patient: benjamin,
+    patient: chris,
     name: "taille"
   },
   {
-    patient: benjamin,
+    patient: chris,
     name: "taille"
   }
 
@@ -123,27 +123,19 @@ allergy_attributes = [  {
 
 Allergy.create!(allergy_attributes)
 
-# antecedent_attributes = [
-#     {
-#     patient: benjamin,
-#     description: "27/10/2016"
-#   },
-#   {
-#     patient: benjamin,
-#     description: ""
-#   },
-#   {
-#     patient: benjamin,
-#     description: "27/10/2017"
-#   },
-#   {
-#     patient: benjamin,
-#     description: "27/10/2016"
-#   }
+antecedent_attributes = [
+    {
+    patient: chris,
+    description: "pollen"
+  },
+  {
+    patient: chris,
+    description: "poils de chatte"
+  }
 
-# ]
+]
 
-# Antecedent.create!(antecedent_attributes)
+Antecedent.create!(antecedent_attributes)
 
 
 
@@ -151,16 +143,36 @@ Allergy.create!(allergy_attributes)
 puts 'Creating episodes...'
 episode_attributes = [
   {
-    patient: benjamin,
-    name: "Douleur en dessous du pied"
+    patient: chris,
+    name: "Fatigue decembre 2017"
   },
   {
-    patient: benjamin,
-    name: "Asthme"
+    patient: chris,
+    name: "Ma santé 0-10 ans"
   },
   {
-    patient: benjamin,
-    name: "Urticaire"
+    patient: chris,
+    name: "Ma santé 10-20 ans"
+  },
+  {
+    patient: chris,
+    name: "Souffle au coeur"
+  },
+  {
+    patient: chris,
+    name: "Entorse cheville 2016"
+  },
+  {
+    patient: chris,
+    name: "Rage de dents 2017"
+  },
+  {
+    patient: chris,
+    name: "Allergies Printemps 2016"
+  },
+  {
+    patient: chris,
+    name: "Grippe 2016"
   }
 ]
 
@@ -175,9 +187,9 @@ caregiver_attributes = [
     address: "46 Bis Boulevard Gabriel Guist'Hau, 44000 Nantes ",
     phone_number: "02 40 20 32 41",
     mail: "marc.greene@gmail.com",
-    speciality: "Medecin généraliste, Docteur du Sport",
-    photo: File.open(Rails.root.join('db/fixtures/images/doctor.png')),
-    patient: benjamin
+    speciality: "Medecin généraliste",
+    photo: File.open(Rails.root.join('db/fixtures/images/doctor-green.png')),
+    patient: chris
   },
   {
     firstname: "Peter",
@@ -186,8 +198,8 @@ caregiver_attributes = [
     phone_number: "02 40 20 03 52",
     mail: "peter.benton@gmail.com",
     speciality: "Radiologue",
-    photo: File.open(Rails.root.join('db/fixtures/images/radiologist.png')),
-    patient: benjamin
+    photo: File.open(Rails.root.join('db/fixtures/images/doctor-green.png')),
+    patient: chris
   },
   {
     firstname: "John",
@@ -195,9 +207,19 @@ caregiver_attributes = [
     address: " 8 Rue Marie Anne du Boccage, 44000 Nantes",
     phone_number: "02 40 69 13 13",
     mail: "john.carter@gmail.com",
-    speciality: "Kiné",
-    photo: File.open(Rails.root.join('db/fixtures/images/kine.png')),
-    patient: benjamin
+    speciality: "Dentiste",
+    photo: File.open(Rails.root.join('db/fixtures/images/doctor-green.png')),
+    patient: chris
+  },
+  {
+    firstname: "Laurent",
+    lastname: "Gabai",
+    address: " 8 Rue Marie Anne du Boccage, 44000 Nantes",
+    phone_number: "02 40 69 13 13",
+    mail: "john.carter@gmail.com",
+    speciality: "Cardiologue",
+    photo: File.open(Rails.root.join('db/fixtures/images/doctor-green.png')),
+    patient: chris
   }
 ]
 
@@ -205,64 +227,98 @@ Caregiver.create!(caregiver_attributes)
 puts "Finished!"
 
 event_attributes = [
-  {
+  {#0
     category: "note",
     description: "Création de l'épisode",
     episode: Episode.first,
-    date: "2017-01-01"
+    date: "2017-12-03"
   },
-  {
+  {#1
     category: "note",
-    description: "J'ai mal au pied. J'ai beau ête matinale, j'ai mal",
+    description: "Fièvre et fatigue depuis 4 jours. 1g efferalgan hier et aujourd'hui.",
     episode: Episode.first,
-    date: "2017-01-02"
+    date: "2017-12-07"
   },
-  {
+  {#2
     category: "appointment",
-    description: "Le docteur m'a donné une ordonnnance pour faire une radio",
+    description: "rdv médecin",
     episode: Episode.first,
-    date: "2017-01-11",
+    date: "2017-12-08",
     caregiver: Caregiver.all[0]
   },
-  {
-    category: "appointment",
-    description: " Il a remarqué une epine calcanéenne sous mon pied gauche. A confirmer avec le docteur Greene",
-    episode: Episode.first,
-    date: "2017-02-01",
-    caregiver: Caregiver.all[1]
-  },
-  {
-    category: "appointment",
-    description: "Le docteur Greene confirme l'épine calcanéenne",
-    episode: Episode.first,
-    date: "2017-02-21",
-    caregiver: Caregiver.all[0]
-  },
-  {
-    category: "appointment",
-    description: "Premier exercice effectué avec le docteur Carter",
-    episode: Episode.first,
-    date: "2017-03-04",
-    caregiver: Caregiver.all[2]
-  },
-  {
-    category: "note",
-    description: "Je commence à aller mieux",
-    episode: Episode.first,
-    date: "2017-05-02"
-      },
-  {
+  {#3
     category: "note",
     description: "Création de l'épisode",
-    episode: Episode.all[1],
-    date: "2016-01-01"
+    episode: Episode.all[5],
+    date: "2017-09-01"
   },
-    {
+  {#4
+    category: "note",
+    description: "Train de fièvre et douleur à la tête",
+    episode: Episode.all[5],
+    date: "2017-08-25"
+  },
+  {#5
+    category: "note",
+    description: "Trop mal à la dent!!! J'ai un rdv cet apremidi chez le dentiste. Petite photo-souvenir :(((",
+    episode: Episode.all[5],
+    date: "2017-09-01"
+  },
+  {#6
+    category: "appointment",
+    description: "Abcès. Traitement de l'abcès. Soulagement dès qu'il a percé l'abcès. Amoxicilline et paracétamol/codeine. J'aurais dû venir plus tôt d'après le dentiste...",
+    episode: Episode.all[5],
+    date: "2017-09-01",
+    caregiver: Caregiver.all[2] #dentiste
+  },
+  {#7
+    category: "note",
+    description: "Ca va beaucoup mieux. J'ai cru mourrir. J'ai douillé!",
+    episode: Episode.all[5],
+    date: "2017-09-07"
+  },
+  {#8
     category: "note",
     description: "Création de l'épisode",
+    episode: Episode.all[3],
+    date: "2014-03-01"
+  },
+  {#9
+    category: "appointment",
+    description: "Visite de routine chez le doc. Je suis assez fatigué en ce moment. Soit c'est la sortie de l'hiver, soit c'est le taff ou peut-etre autre chose. Le dr aimerait que je fasse des examens du coeur car il dit que j'ai un souffle au coeur. Je l'ai depuis longtemps mais il dit qu'il vaut mieux vérifier le coeur tant que je vais bien :). ",
+    episode: Episode.all[3],
+    date: "2017-03-01",
+    caregiver: Caregiver.all[0] #medecin generaliste
+  },
+  {#10
+    category: "appointment",
+    description: "ECG et echographie cardiaque chez le cardiologue. J'avais jamais vu mon coeur!",
+    episode: Episode.all[3],
+    date: "2017-03-10",
+    caregiver: Caregiver.all[3] #cardiologue
+  },
+  {#11
+    category: "appointment",
+    description: "Radio pour compléter le bilan cardiaque. Pas très bavard le radiologue. J'avais l'impression de le déranger ;)",
+    episode: Episode.all[3],
+    date: "2017-03-18",
+    caregiver: Caregiver.all[1] #radiologue
+  },
+  {
+  category: "appointment",
+  description: "Le cardiologue regarde la radio. pas de soucis. C'est un \"souffle fonctionnel\". Ca veut dire que je vais bien, que je fais partie des gens dont le coeur fait des bruits chelous.",
+  episode: Episode.all[3],
+  date: "2017-03-20",
+  caregiver: Caregiver.all[3] #cardiologue
+  },
+     {
+    category: "appointment",
+    description: "Je viens montrer les resultats de mes examens du coeur. Tout est cool. ",
     episode: Episode.all[2],
-    date: "2015-01-01"
-      }
+    date: "2015-03-30",
+    caregiver: Caregiver.all[0] #medecin generaliste
+  }
+
 ]
 
 event = Event.create!(event_attributes)
@@ -270,29 +326,48 @@ event = Event.create!(event_attributes)
 
 document_attributes = [
   {
-    category: "ordonnnance",
-    name: "ordonnnance du Dr",
+    category: "medicament",
+    name: "Ma vieille boite d'efferalgan",
     event: Event.all[1],
-    document: File.open(Rails.root.join('db/fixtures/images/ordonnance.jpg'))
+    document: File.open(Rails.root.join('db/fixtures/images/efferalgan.jpg'))
   },
     {
-    category: "radio",
-    name: "Radio 1",
-    event: Event.all[2],
-    document: File.open(Rails.root.join('db/fixtures/images/radio1.jpg'))
+    category: "photo",
+    name: "oh ma bouche",
+    event: Event.all[5],
+    document: File.open(Rails.root.join('db/fixtures/images/abces-dent.jpg'))
   },
     {
-    category: "radio",
-    name: "Radio 2",
-    event: Event.all[2],
-    document: File.open(Rails.root.join('db/fixtures/images/radio2.jpg'))
+    category: "ordonnance",
+    name: "ordo dentiste",
+    event: Event.all[6],
+    document: File.open(Rails.root.join('db/fixtures/images/ordo-dentiste.jpg'))
   },
-    {
+    {##########
     category: "radio",
-    name: "Radio 3",
-    event: Event.all[2],
-    document: File.open(Rails.root.join('db/fixtures/images/radio3.jpg'))
+    name: "Radio coeur",
+    event: Event.all[11],
+    document: File.open(Rails.root.join('db/fixtures/images/radio-coeur.jpg'))
+  },
+  {
+    category: "ECG",
+    name: "ECG de mon coeur",
+    event: Event.all[10],
+    document: File.open(Rails.root.join('db/fixtures/images/ecg.png'))
+  },
+  {
+    category: "echographie",
+    name: "Echo de mon coeur",
+    event: Event.all[10],
+    document: File.open(Rails.root.join('db/fixtures/images/echo-coeur.jpg'))
+  },
+  {
+    category: "ordonnance",
+    name: "ordo controle coeur",
+    event: Event.all[9],
+    document: File.open(Rails.root.join('db/fixtures/images/ordo-controle-coeur.jpg'))
   }
+
 ]
 
 
