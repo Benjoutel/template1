eaf3b9c079f1c43bd3b4498810a78de82fc74e40
christophe ridarch <christophe.ridarch@gmail.com>
2017-12-13 00:19:10 +0100

    Add feature for adding a caregiver from an episode

--- app/controllers/caregivers_controller.rb
+++ app/controllers/caregivers_controller.rb
@@ -3,8 +3,20 @@ class CaregiversController < ApplicationController
   def create
     @caregiver = Caregiver.new(caregiver_params)
     @caregiver.patient = current_patient
+    if @caregiver.photo.model.id == nil
+      @caregiver.photo = File.open(Rails.root.join("app/assets/images/doctor-green.png"))
+    end
     if @caregiver.save
-      redirect_to episodes_path
+      if caregiver_hidden_params[:from] == "index"
+        redirect_to episodes_path
+      else
+        event = Event.new(category: "appointment", description: "Ajout de l'épisode #{@caregiver.firstname} #{@caregiver.lastname}, #{@caregiver.speciality}", date: Date.today)
+        episode = Episode.find(params[:episode_id])
+        event.episode = episode
+        event.caregiver = @caregiver
+        event.save
+        redirect_to episode_path(episode)
+      end
     else
       render :new
     end
@@ -21,6 +33,10 @@ private
 
   def caregiver_params
     params.require(:caregiver).permit(:firstname, :lastname, :address, :mail, :phone_number, :speciality, :photo)
+  end
+
+  def caregiver_hidden_params
+    params.require(:caregiver).permit(:from)
   end
 end
 
